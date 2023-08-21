8303ec68d867917e2f10a2a5308153948fec3e3e
Kalp <kalpshah2003@gmail.com>
2023-08-20 21:36:58 -0400

    Add comments

--- src/Timer.js
+++ src/Timer.js
@@ -5,19 +5,24 @@ import { useState, useEffect } from 'react'
 
 export default function Timer() {
 
-  const [initialWorkMinutes, setInitialWorkMinutes] = useState(25);
-  const [initialBreakMinutes, setInitialBreakMinutes] = useState(5);
-  const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(initialWorkMinutes);
-  const [displayMessage, setDisplayMessage] = useState(true);
+  const [initialWorkMinutes, setInitialWorkMinutes] = useState(25); // Default number of miniutes for the work session 
+  const [initialBreakMinutes, setInitialBreakMinutes] = useState(5); // Default number of miniutes for the break  session 
+  const [seconds, setSeconds] = useState(0); // Current elapsed seconds 
+  const [minutes, setMinutes] = useState(initialWorkMinutes); // Current elapsed minutes 
+  const [displayMessage, setDisplayMessage] = useState(true); // On-screen element representing the timer 
 
+  // Timer to count down the seconds in real-time 
   useEffect (() => {
     let interval = setInterval(() => {
       clearInterval(interval);
+      // Reset the seconds once they reach 0 
       if (seconds === 0) {
+        // If the timer is not yet done, start the next minute 
         if (minutes !== 0) {
           setSeconds(59);
           setMinutes(minutes - 1);
+
+        // Otherwise change to the next session (break -> work, and vice versa)
         } else {
           let newMinutes = displayMessage ? initialBreakMinutes - 1 : initialWorkMinutes - 1;
           let newSeconds = 59;
@@ -26,14 +31,17 @@ export default function Timer() {
           setDisplayMessage(!displayMessage);
         }
       } else {
+        // Decrease the seconds by 1 
         setSeconds(seconds - 1);
       }
-    }, 1000)
+    }, 1000) // Loop this function every second (1000 ms)
   }, [seconds])
 
+  // Define the string output of the minutes and seconds on the screen 
   const timerMinutes = minutes < 10 ? `0${minutes}` :  minutes;
   const timerSeconds = seconds < 10 ? `0${seconds}` :  seconds;
 
+  // Display the front end 
   return <div className="pomodoro">
     <div className="message">
       {displayMessage && <div>Work Time</div>}
